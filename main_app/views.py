71eb348bb54c305b32761ff11dc9fad9a98fa8a6
platothegeek <platothegeek@gmail.com>
2021-02-04 14:26:47 -0800

    Made basic file structure - currently building out base.html

---
+++ main_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
