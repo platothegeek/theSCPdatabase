9ebd691488a68ce2f1fbaf97feaf625b77793f66
platothegeek <platothegeek@gmail.com>
2021-02-05 11:49:16 -0800

    Started working on views

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('scp/<int:scp_number>/', views.scp_show, name='scp_show'),
-    path('tale/<int:tale_number>/', views.scp_show, name='tale_show'),
+    path('scp/new/', views.scp_new, name='scp_new'),
+    path('tale/<int:tale_id>/', views.tale_show, name='tale_show'),
+    path('tale/new/', views.tale_new, name='tale_show'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -17,3 +17,44 @@ def scp_show(request, scp_number):
     context = { 'scp' : scp }
     print(context)
     return render(request, 'articles/show.html', context)
+def tale_show(request, tale_id):
+    tale = Tales.objects.get(id=tale_id)
+    print(tale.title)
+    context = {'tale' : tale}
+    return render(request, 'tales/show.html', context)
+def scp_new(request):
+    error_message=''
+    if request.method == "POST":
+        form = Scp_Form(request.POST)
+        if form.is_valid():
+            author = 1 #request.user
+            article = Scp
+            article = form.save(commit=False)
+            article.user = author
+            article.save()
+            return redirect('home')
+        else:
+            print(form.errors)
+            error_message = form.errors
+    # current_user = request.user
+    form = Scp_form()
+    contet = {'form' : form, 'error_message': error_message, 'user': current_user}
+    return render(request, 'posts/new.html', context)
+def tale_new(request):
+    error_message=''
+    if requet.method == "POST":
+        form = Tale_Form(request.POST)
+        if form.is_valid():
+            author = 1 #request.user
+            article = Tales
+            article = form.save(commit=False)
+            article.user = author
+            article.save()
+            return redirect('home')
+        else:
+            print(form.errors)
+            error_message = form.errors
+    # current_user = request.user
+    form = Scp_form()
+    contet = {'form' : form, 'error_message': error_message, 'user': current_user}
+    return render(request, 'posts/new.html', context)
