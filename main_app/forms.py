370c61779135d29eb43007c1c38c2ad66a24e9bf
platothegeek <platothegeek@gmail.com>
2021-02-05 11:49:04 -0800

    built out forms

---
+++ main_app/forms.py
@@ -0,0 +1,43 @@
+from django.forms import ModelForm, ModelChoiceField
+from django import forms
+from .models import Scp, Tale, Profile
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from datetime import datetime
+
+class CanonModelChoiceField(ModelChoiceField):
+    def label_from_instance(self, obj):
+        return obj.canon
+class RegisterForm(UserCreationForm):
+    summary = forms.CharField(max_length=250)
+    class Meta:
+        model = Profile
+        fields = ("username", "summary", "email")
+class EditUserForm(UserChangeForm):
+    summary = forms.CharField(max_length=250)
+    username = forms.CharField(max_length=254. required=False)
+    password=None
+    class Meta:
+        model = Profile
+        exclude = ("password1", "password2")
+        fields = ("username", "summary",)
+        help_text=""
+class Scp_Form(ModelForm):
+    title = forms.CharField(max_length=50, required=True)
+    number = forms.IntegerField(required=True, unique=True)
+    body = forms.CharField(max_length=500000, widget=forms.Textarea)
+    canon = CanonModelChoiceField(queryset=Canon.objects.all())
+    class Meta:
+        model = Scp
+        fields=['title','number','canon','body']
+class Edit_Scp_Form(ModelForm):
+    title = forms.CharField(max_length=50, required=True)
+    number = forms.IntegerField(required=True, unique=True)
+    body = forms.CharField(max_length=500000, widget=forms.Textarea)
+    canon = CanonModelChoiceField(queryset=Canon.objects.all())
+    class Meta:
+        model = Scp
+        fields=['title','number','canon','body']
+class Tale_Form(ModelForm):
+    title = forms.CharField(max_length=50)
+    body = forms.CharField(max_length=500000)
+    canon = CanonModelChoiceField(queryset=Canon.objects.all())
