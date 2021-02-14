a5e2bd0701d7334811164e53ee06514efadc390c
platothegeek <platothegeek@gmail.com>
2021-02-14 11:32:41 -0800

    Deploying...

--- SCPdatabase_project/settings.py
+++ SCPdatabase_project/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = "main_app.Profile"
