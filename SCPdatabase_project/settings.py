53842c830e4e6aa9f98b9b94ac33f4ef820c9a9c
platothegeek <73858489+platothegeek@users.noreply.github.com>
2021-02-16 09:50:50 -0800

    Update settings.py

--- SCPdatabase_project/settings.py
+++ SCPdatabase_project/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+o8-4f823bcm1xas6&adxab!qtev^k)5mfsu+caouu&d@u1@f)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
